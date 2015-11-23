5fb75dc31103ae10aede45bcdc661c009552092e
rayline <yrsty@hotmail.com>
2015-11-23 21:08:08 +0800

    looking evolution

--- waterfall.js
+++ waterfall.js
@@ -4,21 +4,21 @@
 
 var presentCnt = 0;
 
-var contentPrest = new Array();
-contentPrest[0]="<img src='images/szt.jpg' width='100%' height='300px' class='insideImg'>";
-contentPrest[1]="<img src='images/xyz.jpg' width='100%' height='250px' class='insideImg'>";
-contentPrest[2]="<img src='images/dbw.jpg' width='100%' height='370px' class='insideImg'>";
+var contentPreset = new Array();
+contentPreset[0]="<img src='images/szt.jpg' width='100%' height='300px' class='insideImg'>";
+contentPreset[1]="<img src='images/xyz.jpg' width='100%' height='250px' class='insideImg'>";
+contentPreset[2]="<img src='images/dbw.jpg' width='100%' height='370px' class='insideImg'>";
 
 function resizeMain(){
     wid = $("body").width();
-    cnt = Math.floor(wid*0.8 / 165);
+    cnt = Math.floor(wid*0.95 / 165);
     $("#columnContainer").css("margin-left", (wid-cnt*165)/2);
     while(cnt>presentCnt){
         presentCnt++;
         $("#columnContainer").append("<div class='column column"+presentCnt +"'></div>");
         for(var i=1;i<=10;i++){
-            rd = Math.floor(Math.random()*3);
-            $(".column"+presentCnt).append(contentPrest[rd]);
+            rd = Math.floor(Math.random()*contentPreset.length);
+            $(".column"+presentCnt).append(contentPreset[rd]);
         }
     }
     while(cnt<presentCnt){
